8568b971ed112b0be4f455416c9a724d41869c8f
Boris Filin <u7637604@anu.edu.au>
2025-05-08 20:04:25 +0100

    Document Group associated fields

---
+++ NeditGD/Dictionaries/logic_IDs.py
@@ -0,0 +1,35 @@
+'''
+This file includes various info regarding GD triggers. This should help with high-tech level creation.
+
+Logic includes anything that can be remapped
+- groups
+- counter/timer IDs
+- collision IDs
+'''
+
+
+GROUP_FIELDS = [
+    'target',
+    'target_pos',
+    'center_group_id',
+    'rotation_target',
+    'gradient_bl',
+    'gradient_br',
+    'gradient_tl',
+    'gradient_tr'
+]
+
+ITEM_FIELDS = [
+    'target', # For item edit only
+    'item_id',
+    'block_b'
+]
+
+COLOR_FIELDS = [
+    'target'
+]
+
+COLLISION_FIELDS = [
+    'item_id',
+    'block_b'
+]
